176d84d863a799e73f38eb4be4cc8a43d55405fa
Almir Campos <acampos@bethel-guest.dhcp-216-230.vlan.net.its.bethel.edu>
2016-12-12 21:27:59 -0800

    p01, l01 - ijm-basics - the first microservice

---
+++ ijm-basics/src/main/java/com/javasd/ijm/basics/IjmBasicsApplication.java
@@ -0,0 +1,14 @@
+package com.javasd.ijm.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjmBasicsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IjmBasicsApplication.class, args);
+    }
+}
