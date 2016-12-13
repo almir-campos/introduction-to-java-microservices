cbbf08625936d551829bd61e7dadaaec0539ec4b
Almir Campos <acampos@acampos-4.local>
2016-12-13 14:34:15 -0800

    l01, p04 - Sending/Receiving objects to/from the Swagger2 front-end

--- ijm-basics/src/main/java/com/javasd/ijm/basics/controller/BasicsController.java
+++ ijm-basics/src/main/java/com/javasd/ijm/basics/controller/BasicsController.java
@@ -6,8 +6,10 @@
 package com.javasd.ijm.basics.controller;
 
 import com.javasd.ijm.commons.misc.Msg;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -36,6 +38,18 @@ public class BasicsController
     public Object msgGet()
     {
         return new Msg( "Sending an object!");
+    }
+    
+    @RequestMapping( value = "/echoGet", method = RequestMethod.GET )
+    public Object echoGet( @RequestParam Msg msg )
+    {
+        return msg;
+    }
+    
+    @RequestMapping( value = "/echoPost", method = RequestMethod.POST )
+    public Object echoPost( @RequestBody Msg msg )
+    {
+        return msg;
     }
     
 }
