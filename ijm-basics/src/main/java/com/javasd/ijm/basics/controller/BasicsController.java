c916c98672b4a26fe6b44b1c058e20250ecb13d9
Almir Campos <acampos@bethel-guest.dhcp-216-230.vlan.net.its.bethel.edu>
2016-12-12 21:41:31 -0800

    p01, l01 - Sending an object to the client

--- ijm-basics/src/main/java/com/javasd/ijm/basics/controller/BasicsController.java
+++ ijm-basics/src/main/java/com/javasd/ijm/basics/controller/BasicsController.java
@@ -16,9 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicsController
 {
+
+    /**
+     *
+     * @return
+     */
     @RequestMapping( value = "/", method = RequestMethod.GET )
     public Object root()
     {
         return "Basics Microservices - Up and Running!";
     }
+    
+    /**
+     *
+     * @return
+     */
+    @RequestMapping( value = "/msgGet", method = RequestMethod.GET )
+    public Object msgGet()
+    {
+        return new Msg( "Sending an object!");
+    }
+    
+}
+
+class Msg
+{
+    private final String msg;
+
+    public Msg(String msg)
+    {
+        this.msg = msg;
+    }
+
+    public String getMsg()
+    {
+        return msg;
+    }
+    
 }
