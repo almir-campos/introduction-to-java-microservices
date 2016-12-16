b9184c2e784b510b1c8878dcf2f646bca5d406a8
Almir Campos <acampos@acampos-4.local>
2016-12-15 16:05:32 -0800

    p02, l05 - Exercise: exam_id

---
+++ ijm-exam/src/main/java/com/javasd/ijm/exam/service/ExamService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javasd.ijm.exam.service;
+
+import com.javasd.ijm.exam.repository.ExamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author acampos
+ */
+@Service
+public class ExamService
+{
+    @Autowired
+    private ExamRepository examRepository;
+            
+    public Object findAll()
+    {
+        return examRepository.findAll();
+    }
+}
