ef0ef2b99e678d546f20b0b037f90bae3afc0884
Almir Campos <acampos@acampos-4.local>
2016-12-18 20:46:05 -0800

    p02, l02 - QnA basic microservice

---
+++ ijm-qna/src/test/java/com/javasd/ijm/qna/IjmQnaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javasd.ijm.qna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IjmQnaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
