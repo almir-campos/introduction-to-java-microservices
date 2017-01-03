28bf1ddc8e628f43afcc589e60153e00a3341b92
Almir Campos <acampos@bethel-guest.dhcp-216-110.vlan.net.its.bethel.edu>
2017-01-02 16:05:54 -0800

    p08, l07 - Get properties from default properties

--- ijm-ui/src/main/java/com/javasd/ijm/ui/IjmUiApplication.java
+++ ijm-ui/src/main/java/com/javasd/ijm/ui/IjmUiApplication.java
@@ -1,10 +1,12 @@
 package com.javasd.ijm.ui;
 
 import com.javasd.ijm.commons.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,6 +18,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class IjmUiApplication implements CommandLineRunner
 {
+    @Autowired
+    private Environment environment;
 
     /**
      *
@@ -35,7 +39,10 @@ public class IjmUiApplication implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception
     {
-        Utils.consoleMsg("UI Microservice up and running!");
+        String serverPort = environment.getProperty("server.port");
+        String managementPort = environment.getProperty("management.port");
+        Utils.consoleMsg("UI Microservice up and running at port " + serverPort
+        + " (Please, use the port " + managementPort + " for the Actuator endpoints)");
     }
     
     /**
