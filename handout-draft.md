0b0e30ad02b93c6dbb618f781f459c7fd380da16
Almir Campos <almircampos@gmail.com>
2017-08-11 21:48:55 -0700

    Part 01 - Lesson 04
    * Resolving conflicts

---
+++ handout-draft.md
@@ -0,0 +1,4 @@
+# Part 01 - Lesson 02
+
+* Sending an object to the client
+* Changing the listening port to 9000
--- ijm-basics/src/main/java/com/javasd/ijm/basics/controller/BasicsController.java
+++ ijm-basics/src/main/java/com/javasd/ijm/basics/controller/BasicsController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.javasd.ijm.basics.controller;
 
 import com.javasd.ijm.commons.misc.Msg;
