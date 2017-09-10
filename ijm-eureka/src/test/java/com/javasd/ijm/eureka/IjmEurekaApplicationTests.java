8dfb39798eb13e8d8e08a77a555238665e4005c7
Almir Campos <almircampos@gmail.com>
2017-09-09 22:38:29 -0700

    p09-l01-Added Eureka basic server

---
+++ ijm-eureka/src/test/java/com/javasd/ijm/eureka/IjmEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javasd.ijm.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IjmEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
